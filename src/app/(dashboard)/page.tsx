f0d39c0882c4b125b9b225855cf7915253864b77
omar-mandour <mandouro383@gmail.com>
2025-04-26 11:24:06 +0300

    يارب المشكله تتحل

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,35 +1,34 @@
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
-// import { getCurrent } from "@/feature/auth/query";
-// import { getWorkspaces } from "@/feature/workspaces/query";
-// import { redirect } from "next/navigation";
+import { getCurrent } from "@/feature/auth/query";
+import { getWorkspaces } from "@/feature/workspaces/query";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  // console.log("===> [Dashboard] Trying to fetch current user...");
+  console.log("===> [Dashboard] Trying to fetch current user...");
 
-  // const user = await getCurrent();
-  // console.log("===> [Dashboard] Current user:", user);
+  const user = await getCurrent();
+  console.log("===> [Dashboard] Current user:", user);
 
-  // if (!user) {
-  //   console.log("===> [Dashboard] No user found, redirecting to /sign-in");
-  //   return redirect("/sign-in");
-  // }
+  if (!user) {
+    console.log("===> [Dashboard] No user found, redirecting to /sign-in");
+    return redirect("/sign-in");
+  }
 
-  // console.log("===> [Dashboard] Fetching workspaces...");
-  // const workspace = await getWorkspaces();
-  // console.log("===> [Dashboard] Workspaces fetched:", workspace);
+  console.log("===> [Dashboard] Fetching workspaces...");
+  const workspace = await getWorkspaces();
+  console.log("===> [Dashboard] Workspaces fetched:", workspace);
 
-  // if (!workspace.documents?.[0]?.$id || workspace.total === 0) {
-  //   console.log(
-  //     "===> [Dashboard] No workspace found, redirecting to /workspaces/create"
-  //   );
-  //   return redirect("/workspaces/create");
-  // }
+  if (!workspace.documents?.[0]?.$id || workspace.total === 0) {
+    console.log(
+      "===> [Dashboard] No workspace found, redirecting to /workspaces/create"
+    );
+    return redirect("/workspaces/create");
+  }
 
-  // console.log(
-  //   `===> [Dashboard] Redirecting to /workspaces/${workspace.documents[0].$id}`
-  // );
-  // return redirect(`/workspaces/${workspace.documents[0].$id}`);
-  return <div>hello world</div>;
+  console.log(
+    `===> [Dashboard] Redirecting to /workspaces/${workspace.documents[0].$id}`
+  );
+  return redirect(`/workspaces/${workspace.documents[0].$id}`);
 }
